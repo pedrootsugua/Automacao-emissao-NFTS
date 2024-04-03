e7b3a59ac738ff1bf002f5974347b46cd35e0eb2
vituapds <162765216+vituapds@users.noreply.github.com>
2024-04-03 01:32:45 -0300

    Correção de erros e modelagem no arquivo NFTS_Launcher.py
    Peguei todos os dados e corrigi novamente validando se os espaços no txt estavam de acordo com o layout da prefeitura e coloquei comentarios em todas as etapas que utilizamos

--- NFTS_Launcher.py
+++ NFTS_Launcher.py
@@ -1,72 +1,103 @@
+##################### Importing all the necessary libraries ##############################
 import pandas as pd
 from datetime import datetime
 import os
+
+def format_number(x, zeros=15):
+    rounded_number = round(float(x), 2)
+    formatted_number = '{:.2f}'.format(rounded_number)
+    padded_number = formatted_number.replace('.', '').zfill(zeros)
+    return padded_number
+
+##################### Coletting all the paths necessary ##############################
 
 Wb_Tomadora_Path = fr'./Dados/Target_Workbook.xlsx'
 Wb_Tomadora = pd.read_excel(Wb_Tomadora_Path, sheet_name='Dados_Tomadora')
 Wb_Info = pd.read_excel(Wb_Tomadora_Path, sheet_name='Dados_NF')
 
+##################### Extracting data from Cabeçalho ##############################
 
-# ##### Extracting data from Cabeçalho
-
-
-Tipo_Registro = '1'
-Versao = '001'
-Inscricao_Mun = Wb_Tomadora.iloc[1, 1]
-Data_Inicio = pd.to_datetime(Wb_Tomadora.iloc[1, 2], format='%d/%m/%Y').strftime('%Y%m%d')
+TR_CB = '1' # 1)
+Versao = '001' # 2)
+Inscricao_Mun = Wb_Tomadora.iloc[1, 1] # 3)
+Data_Inicio = pd.to_datetime(Wb_Tomadora.iloc[1, 2], format='%d/%m/%Y').strftime('%Y%m%d') # 4)
 Data_Fim = pd.to_datetime(Wb_Tomadora.iloc[1, 3], format='%d/%m/%Y').strftime('%Y%m%d')
 
-### passing everything to string type
-Tipo_Registro = str(Tipo_Registro)
-Versao = str(Versao)
-Inscricao_Mun = str(Inscricao_Mun)
-Data_Inicio = str(Data_Inicio)
-Data_Fim = str(Data_Fim)
+##################### Getting Cabecalho in one variable ##############################
 
-# Getting everything in one variable
 Cabeca = (
-    Tipo_Registro +
+    TR_CB +
     Versao +
-    Inscricao_Mun +
+    str(Inscricao_Mun) +
     Data_Inicio +
     Data_Fim
 )
 
+##################### Extracting NF Info's from lines #############################
 
-### Extracting data from NF's Details
 ID_pagamento = Wb_Info['ID_pagamento'].astype(str).str.replace('.', '')
 
-TR_NF = '4'
+TR_NF = '4' # 1)
+Tipo_Documento = '02' # 2)
+Serie_NF = '0' * 5 # 3)
+Numero_NF = Wb_Info['Numero_NF'].astype(str).str.zfill(12) # 4)
+Data_NF = str(pd.to_datetime(Wb_Info['Data_Prestação'], format='%d/%m/%Y').dt.strftime('%Y%m%d')) # 5)
 
-Tipo_Documento = '02' # Ponto importante de se fazer um if/else caso vá realizar transmissão em lote de câmbios.
+Situacao_NF = 'N' # 6)
+# N = Normal
+# C = Cancelado
 
-Serie_NF = '0' * 5
-Data_NF = pd.to_datetime(Wb_Info['Data_Prestação'], format='%d/%m/%Y').dt.strftime('%Y%m%d')
-Numero_NF = Wb_Info['Numero_NF'].astype(str).str.zfill(12)
-Situacao_NF = 'N'
-Tributacao = Wb_Info['Tributacao']
-Valor_Servico = Wb_Info['Valor_Servico'].astype(str).str.replace(',', '').str.replace('.', '').str.zfill(15)
-Valor_Deducoes = Wb_Info['Valor_Deducoes'].astype(str).str.replace(',', '').str.replace('.', '').str.zfill(15)
-Cod_Serv = Wb_Info['Cod_Serv'].astype(str).str.zfill(5)
-Cod_Subitem = Wb_Info['Cod_Subitem'].astype(str).str.replace('.', '').str.zfill(4)
-Aliquota = Wb_Info['Aliquota'].astype(str).str.replace(',', '').str.replace('%', '').str.zfill(4)
-ISS_Retido = Wb_Info['ISS_Retido'].astype(str) # 1 = retido pelo tomador / 2 = Sem retenção / 4 = Retido pelo Tomador
-Indica_CNPJ= '2' # Emissor é cnpj, sempre tem que ser 2
-CNPJ = Wb_Info['CNPJ'].astype(str).str.replace('-', '').str.replace('.', '').str.replace('/', '')
-CCM_Prestador = '0' * 8
-Razao_social = '0' * 75
-# sequencia pra completar
-Endereco = '0'*123
-Cidade_Prestador = Wb_Info['Cidade_Prestador'].str.rjust(50)
-UF_Prestador = Wb_Info['UF_Prestador']
-CEP_Prestador = Wb_Info['CEP_Prestador'].astype(str).str.replace('-', '')
-Email = ' ' * 75
-Tipo_NFTS = '1'
-Regime = '0'
-Dt_Pgto = '0' * 8
-Descriminacao = Wb_Info['Descriminacao']
+Tributacao = Wb_Info['Tributacao'] # 7)
+# T - Operação normal
+# I - Imune
+# J – ISS Suspenso por Decisão Judicial
 
-# Getting everything in one variable
+# Treating values
+Valor_Servico = Wb_Info['Valor_Servico'].apply(format_number, zeros=15) # 8) - Using the def we created to format the values in R$
+Valor_Deducoes = Wb_Info['Valor_Deducoes'].apply(format_number, zeros=15) # 9)
+
+Cod_Serv = Wb_Info['Cod_Serv'].astype(str).str.zfill(5) # 10
+Cod_Subitem = Wb_Info['Cod_Subitem'].astype(str).str.replace('.', '').str.zfill(4) # 11)
+Aliquota = Wb_Info['Aliquota'].astype(str).str.replace(',', '').str.replace('%', '').str.zfill(4) # 12)
+
+ISS_Retido = str(Wb_Info['ISS_Retido']) # 13)
+# 1 – ISS Retido pelo tomador.
+# 2 – NFTS sem ISS Retido.
+# 3 – ISS Retido pelo intermediário.
+# 4 – ISS Retido pelo tomador (descumprimento do Art. 8º A, §1º, da Lei Complementar 116, de 31 de julho de 2003)
+# 5 – ISS Retido pelo intermediário (descumprimento do Art. 8º A, §1º, da Lei Complementar 116, de 31 de julho de 2003)
+
+Indica_CNPJ= '2' # 14)
+# 1 para CPF.
+# 2 para CNPJ.
+# 3 para Prestador estabelecido no exterior.
+
+CNPJ = Wb_Info['CNPJ'].astype(str).str.replace('-', '').str.replace('.', '').str.replace('/', '') # 15)
+
+CCM_Prestador = '0' * 8 # 16)
+Razao_social = '0' * 75 # 17)
+
+# Sequency to complete all the empty spaces in the code
+Endereco = ' ' * 123 # 18,19,20,21,22)
+
+Cidade_Prestador = Wb_Info['Cidade_Prestador'].str.rjust(50) # 23)
+UF_Prestador = Wb_Info['UF_Prestador'] # 24
+CEP_Prestador = Wb_Info['CEP_Prestador'].astype(str).str.replace('-', '') # 25
+Email = ' ' * 75 # 26
+
+Tipo_NFTS = '1' # 27
+# 1 - Nota Fiscal do Tomador;
+# 2 - Nota Fiscal do Intermediário.
+
+Regime = '0' # 28
+# 0 – Normal ou Simples Nacional (DAMSP);
+# 4 – Simples Nacional (DAS);
+# 5 – Microempreendedor Individual - MEI.
+
+Dt_Pgto = ' ' * 8 # 29
+Descriminacao = Wb_Info['Descriminacao'] # 30
+
+##################### Getting NF Info's in one variable ##############################
 
 Info_nf = (
     TR_NF +
@@ -97,44 +128,35 @@ Info_nf = (
     Descriminacao
 )
 
-# Extracting rodapé
-Tipo_Registro_RP = '9'
+##################### Extracting rodapé #############################
 
-#CRIAR UM COUNT PARA A QTD DE NOTAS EXISTENTES NO TXT
-Wb_Info_Value = str(len(Wb_Info['ID_pagamento'])).zfill(7) #(VERIFICAR) DEVE SER INFORMADO A QTD DE LINHAS(DADOS NFE) QUE EXISTEM NO TIPO 4(LINHAS DE DETALHE)
+TR_RP = '9'
 
-# formatação valor deduções
+Wb_Info_Value = str(len(Wb_Info['ID_pagamento'])).zfill(7) # Contador de linhas
+
 Soma_Valor_Deducoes = float(Wb_Info['Valor_Deducoes'].sum())
-Soma_Valor_Deducoes_format = format(Soma_Valor_Deducoes, '.2f')
-Soma_Valor_Deducoes_format = Soma_Valor_Deducoes_format.replace(',','').replace('.','').zfill(15)
-print(Soma_Valor_Deducoes)#VERIFICAR CALCULO DA SOMA
+Soma_Valor_Deducoes_format = '{:016.2f}'.format(round(Soma_Valor_Deducoes, 2)).replace('.', '').replace(',', '') # Valor da coluna Deduções somados
 
-# formatação valor serviço
 Soma_Valor_Servico = float(Wb_Info['Valor_Servico'].sum())
-Soma_Valor_Servico_format = format(Soma_Valor_Servico, '.2f')
-Soma_Valor_Servico_format = Soma_Valor_Servico_format.replace(',','').replace('.','').zfill(15)
-print(Soma_Valor_Servico)#VERIFICAR CALCULO DA SOMA
+Soma_Valor_Servico_format = '{:016.2f}'.format(round(Soma_Valor_Servico, 2)).replace('.', '').replace(',', '') # Valor da coluna Serviços somados
 
-# Passing everything to string type
-Tipo_Registro_RP = str(Tipo_Registro_RP)
-Wb_Info_Value = str(Wb_Info_Value)
-Soma_Valor_Servico = str(Soma_Valor_Servico)
-Soma_Valor_Deducoes = str(Soma_Valor_Deducoes)
+##################### Getting Rodapé Infos in one variable ##############################
 
-
-# Getting everything in one variable
 Rodape = (
-    Tipo_Registro_RP +
+    TR_RP +
     Wb_Info_Value +
-    Soma_Valor_Servico +
-    Soma_Valor_Deducoes
+    Soma_Valor_Deducoes_format +
+    Soma_Valor_Servico_format
 )
 
-# Making our .Txt file
+
+####################### Making .Txt file ##############################
+
 data_atual = datetime.now()
 cont = 1
 nome_base = 'Lote_NFTS_' + data_atual.strftime("%Y-%m-%d")
 nome_arquivo = f"{nome_base}_{cont}.txt"
+
 
 while os.path.exists(nome_arquivo):
     cont += 1
